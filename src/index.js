bba06c95af71e271b211809a9c78ca12d95d6c91
Kenny Okada <kokada32@gmail.com>
2021-08-31 21:47:15 -0700

    passed props to CommentDetail component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content="This is a blog post!" avatar={faker.image.avatar()} />
+            <CommentDetail author="Alex" timeAgo="Today at 2:00AM" content="Happy blogging!" avatar={faker.image.avatar()} />
+            <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" content="Blog me!" avatar={faker.image.avatar()} />
         </div>
     );
 };
